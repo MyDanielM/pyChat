6f891f4e724ace3946b7c6e41b9695eba7149633
myDanielM <godinseccoming@gmail.com>
2022-06-21 11:51:52 +0300

    add main functional

--- client/client.py
+++
@@ -1,9 +0,0 @@
-import socket, threading
-
-name = input('Enter your name: ')
-
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-host = input('Enter server addr: ')
-port = int(input('Enter server port: '))
-client.connect((host, port))
-
---
+++ server.py
@@ -0,0 +1,39 @@
+import socket
+from threading import Thread
+
+SERVER_HOST = "127.0.0.1"
+SERVER_PORT = 8080
+separator_token = "<SEP>"
+
+client_sockets = set()
+server = socket.socket()
+server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+server.bind((SERVER_HOST, SERVER_PORT))
+server.listen(5)
+print(f"[*] Listening as {SERVER_HOST}:{SERVER_PORT}")
+
+
+def listen_for_client(cs):
+    while True:
+        try:
+            msg = cs.recv(1024).decode()
+        except Exception as e:
+            print(f"[!] Error: {e}")
+            client_sockets.remove(cs)
+        else:
+            msg = msg.replace(separator_token, ": ")
+        for client_socket in client_sockets:
+            client_socket.send(msg.encode())
+
+
+while True:
+    client_socket, client_address = server.accept()
+    print(f"[+] {client_address} connected.")
+    client_sockets.add(client_socket)
+    t = Thread(target=listen_for_client, args=(client_socket,))
+    t.daemon = True
+    t.start()
+
+for cs in client_sockets:
+    cs.close()
+s.close()
